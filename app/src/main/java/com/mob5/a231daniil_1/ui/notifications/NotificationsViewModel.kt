90e3d0035082d564a48744293212cbb6309a210d
user <mob5@wsr.ru>
2022-06-02 09:28:01 +0500

    aa

---
+++ app/src/main/java/com/mob5/a231daniil_1/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.mob5.a231daniil_1.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
