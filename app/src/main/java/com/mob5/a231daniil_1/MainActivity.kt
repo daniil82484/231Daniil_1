bfbdeb50c0d6dabe1b92e85a201298f7aaf9a460
user <mob5@wsr.ru>
2022-06-02 08:34:33 +0500

    Initial commit

---
+++ app/src/main/java/com/mob5/a231daniil_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mob5.a231daniil_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
